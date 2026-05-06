e7b92d269877b990afd5306f0a2e6394c6405a36
Mahmoud Elashwah <mahmoud28841@gmail.com>
2026-05-07 02:08:48 +0300

    fix bugs

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = require("./src/app");
 const connectDB = require("./src/config/db");
 const http = require("http");
 const { Server } = require("socket.io");
-const initializeSocket = require("./src/config/socket");   
+const initializeSocket = require("./src/config/socket");
 require("dotenv").config();
 
 const PORT = process.env.PORT || 3000;
@@ -14,13 +14,13 @@ const server = http.createServer(app);
 // Initialize Socket.io
 const io = new Server(server, {
   cors: {
-    origin: "*",                    
+    origin: "*",
     methods: ["GET", "POST"],
     credentials: true,
   },
   pingTimeout: 60000,
-  pingInterval: 25000, 
-});   
+  pingInterval: 25000,
+});
 
 // Initialize Socket handlers
 initializeSocket(io);
@@ -31,7 +31,7 @@ app.set("io", io);
 // Connect to Database then Start Server
 connectDB().then(() => {
   server.listen(PORT, () => {
-    console.log(`🚀 Server is running on http://localhost:${PORT}`);
+    console.log(`🚀 Server is running on port:${PORT}`);
     console.log(`📡 Socket.io is listening`);
   });
 });
--- src/config/passport.js
+++ src/config/passport.js
@@ -1,12 +1,21 @@
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+
+let url;
+if (process.env.NODE_ENV === "development") {
+  url = "http://localhost:3000";
+} else if (process.env.NODE_ENV === "production") {
+  url = "https://dentalclinicbackend-production.up.railway.app";
+} else {
+  url = "http://localhost:3000";
+}
 
 passport.use(
   new GoogleStrategy(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "http://localhost:3000/api/v1/oauth/google/callback",
+      callbackURL: `${url}/api/v1/oauth/google/callback`,
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
@@ -14,13 +23,13 @@ passport.use(
           accessToken,
           refreshToken,
           profile,
-        };  
+        };
         return done(null, user);
       } catch (err) {
         return done(err, null);
       }
-    }
-  )
+    },
+  ),
 );
 
 module.exports = passport;
